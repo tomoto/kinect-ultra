cb93fee26b162800ea54b87ecad109909f6ef27b
tomoto <tomoto@71011c23-f5df-4f85-535c-b37ab9e39a43>
2011-01-18 06:52:57 +0000

    Added tools. Requires ActivePerl.

---
+++ trunk/tool/package-release.bat
@@ -0,0 +1,15 @@
+@rem Note: (1) Set VER to release version. (2) Install ActivePerl.
+
+setlocal
+set VER=0.1c
+set TARGET=release\kinect-ultra-%VER%
+rmdir /s /q %TARGET%
+mkdir %TARGET%
+xcopy ..\data %TARGET%\data\ /e
+copy ..\build\release\kinect-ultra.exe %TARGET%\
+copy ..\build\release\cxcore210.dll %TARGET%
+copy ..\build\release\highgui210.dll %TARGET%
+copy ..\LICENSE.TXT %TARGET%
+copy ..\README_*.TXT %TARGET%
+copy ..\HISTORY_*.TXT %TARGET%
+perl zip.pl %TARGET%
