91e24ff65150b28eac881dd5cf36b5185813ab08
tomoto <tomoto@71011c23-f5df-4f85-535c-b37ab9e39a43>
2012-04-23 10:26:28 +0000

    Fixed release package command.

--- tool/package-release.bat
+++ tool/package-release.bat
@@ -2,14 +2,26 @@
 
 setlocal
 set VER=1.0a
-set TARGET=release\kinect-ultra_%VER%
+
+set TARGET=release\kinect-ultra_%VER%_for_KinectSDK
 rmdir /s /q %TARGET%
 mkdir %TARGET%
 xcopy ..\data %TARGET%\data\ /e
-copy ..\build\release\kinect-ultra.exe %TARGET%\
-copy ..\build\release\cxcore210.dll %TARGET%
-copy ..\build\release\highgui210.dll %TARGET%
+copy ..\build2010\MSSDK_Release\kinect-ultra.exe %TARGET%\
+copy ..\build2010\MSSDK_Release\opencv_*.dll %TARGET%
 copy ..\LICENSE.TXT %TARGET%
 copy ..\README_*.TXT %TARGET%
 copy ..\HISTORY_*.TXT %TARGET%
 perl zip.pl %TARGET%
+
+set TARGET=release\kinect-ultra_%VER%_for_OpenNI
+rmdir /s /q %TARGET%
+mkdir %TARGET%
+xcopy ..\data %TARGET%\data\ /e
+copy ..\build2010\OpenNI_Release\kinect-ultra.exe %TARGET%\
+copy ..\build2010\OpenNI_Release\opencv_*.dll %TARGET%
+copy ..\LICENSE.TXT %TARGET%
+copy ..\README_*.TXT %TARGET%
+copy ..\HISTORY_*.TXT %TARGET%
+perl zip.pl %TARGET%
+
