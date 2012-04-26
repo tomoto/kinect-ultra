5d71d5ca76e6308c38b51cd90f5f0126a75aebc0
tomoto <tomoto@71011c23-f5df-4f85-535c-b37ab9e39a43>
2012-04-26 07:50:29 +0000

    Create tag v1.0a

---
+++ tags/v1.0a/src/AbstractElementRenderer.h
@@ -0,0 +1,112 @@
+//@COPYRIGHT@//
+//
+// Copyright (c) 2011, Tomoto S. Washio
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of the Tomoto S. Washio nor the names of his
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL TOMOTO S. WASHIO BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//@COPYRIGHT@//
+
+#ifndef _ABSTRACT_ELEMENT_RENDERER_H_
+#define _ABSTRACT_ELEMENT_RENDERER_H_
+
+#include "common.h"
+#include "vec.h"
+#include "AbstractOpenGLRenderer.h"
+#include "TimeTicker.h"
+#include <GLFrame.h>
+#include <list>
+
+struct AbstractElementRendererElement
+{
+	XV3 p; // position
+	XV3 v; // forward vector
+	float lifeTime; // life time left
+};
+
+template <class ElementType> class AbstractElementRenderer : public AbstractOpenGLRenderer
+{
+protected:
+	GLuint m_textureID;
+	TimeTicker m_ticker;
+	std::list<ElementType> m_elements;
+
+	float m_gravity;
+
+public:
+	AbstractElementRenderer(RenderingContext* rctx, const char* alphaTextureFile, float gravity)
+		: AbstractOpenGLRenderer(rctx)
+	{
+		m_textureID = readAlphaTexture(alphaTextureFile);
+		m_gravity = gravity;
+	}
+
+	virtual ~AbstractElementRenderer()
+	{
+	}
+
+	virtual void draw()
+	{
+		executeDraw();
+
+		float dt = m_ticker.tick();
+		if (dt > 0.0f) {
+			progress(dt);
+		}
+	}
+
+protected:
+	void setupTextureParameters()
+	{
+		glEnable(GL_TEXTURE_2D);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glBindTexture(GL_TEXTURE_2D, m_textureID);
+	}
+
+	virtual void executeDraw() = 0;
+
+	void progress(float dt)
+	{
+		std::list<ElementType>::iterator i = m_elements.begin();
+		while (i != m_elements.end()) {
+			i->p += i->v * dt;
+			i->v.Y -= m_gravity * dt;
+
+			onProgress(&(*i), dt);
+
+			i->lifeTime -= dt;
+			if (i->lifeTime <= 0.0f) {
+				i = m_elements.erase(i);
+			} else {
+				i++;
+			}
+		}
+	}
+
+	virtual void onProgress(ElementType* element, float dt)
+	{
+	}
+};
+
+#endif
---
+++ tags/v1.0a/src/SkeletonRenderer.cpp
@@ -0,0 +1,106 @@
+//@COPYRIGHT@//
+//
+// Copyright (c) 2011, Tomoto S. Washio
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of the Tomoto S. Washio nor the names of his
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL TOMOTO S. WASHIO BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//@COPYRIGHT@//
+
+#include "SkeletonRenderer.h"
+#include "util.h"
+
+SkeletonRenderer::SkeletonRenderer(RenderingContext* rctx, DepthProvider* depthProvider, HenshinDetector* henshinDetector)
+: AbstractOpenGLRenderer(rctx)
+{
+	m_depthProvider = depthProvider;
+	m_henshinDetector = henshinDetector;
+}
+
+SkeletonRenderer::~SkeletonRenderer()
+{
+}
+
+void SkeletonRenderer::draw()
+{
+	if (m_henshinDetector->getUserDetector()->getTrackedUserID() == 0) {
+		return;
+	}
+
+	if (m_henshinDetector->getStage() == HenshinDetector::STAGE_HUMAN) {
+		return; // now human
+	}
+
+	if (m_henshinDetector->getStage() == HenshinDetector::STAGE_DEHENSHINING) {
+		return; // now flying out
+	}
+
+	glDisable(GL_DEPTH_TEST);
+	glLineWidth(getPointSize()*2);
+	glPointSize(getPointSize()*4);
+
+	float color[] = { 0.7f, 0.7f, 0.7f, 0.7f};
+	m_rctx->shaderMan->UseStockShader(GLT_SHADER_FLAT, m_rctx->transform.GetModelViewProjectionMatrix(), color);
+
+	// drawBone(userID, XU_SKEL_NECK, XU_SKEL_HEAD);
+
+	drawBone(XU_SKEL_NECK, XU_SKEL_LEFT_SHOULDER);
+	drawBone(XU_SKEL_LEFT_SHOULDER, XU_SKEL_LEFT_ELBOW);
+	drawBone(XU_SKEL_LEFT_ELBOW, XU_SKEL_LEFT_HAND);
+
+	drawBone(XU_SKEL_NECK, XU_SKEL_RIGHT_SHOULDER);
+	drawBone(XU_SKEL_RIGHT_SHOULDER, XU_SKEL_RIGHT_ELBOW);
+	drawBone(XU_SKEL_RIGHT_ELBOW, XU_SKEL_RIGHT_HAND);
+
+	drawBone(XU_SKEL_NECK, XU_SKEL_TORSO);
+
+	drawBone(XU_SKEL_TORSO, XU_SKEL_LEFT_HIP);
+	drawBone(XU_SKEL_LEFT_HIP, XU_SKEL_LEFT_KNEE);
+	drawBone(XU_SKEL_LEFT_KNEE, XU_SKEL_LEFT_FOOT);
+
+	drawBone(XU_SKEL_TORSO, XU_SKEL_RIGHT_HIP);
+	drawBone(XU_SKEL_RIGHT_HIP, XU_SKEL_RIGHT_KNEE);
+	drawBone(XU_SKEL_RIGHT_KNEE, XU_SKEL_RIGHT_FOOT);
+
+	glEnable(GL_DEPTH_TEST);
+}
+
+void SkeletonRenderer::drawBone(XuSkeletonJointIndex fromJoint, XuSkeletonJointIndex toJoint)
+{
+	XuSkeletonJointInfo fromPos, toPos;
+	m_henshinDetector->getUserDetector()->getSkeletonJointInfo(fromJoint, &fromPos);
+	m_henshinDetector->getUserDetector()->getSkeletonJointInfo(toJoint, &toPos);
+
+	if (isConfident(fromPos) && isConfident(toPos)) {
+		glBegin(GL_LINES);
+		glVertex3fv(XV3toM3D(fromPos.position));
+		glVertex3fv(XV3toM3D(toPos.position));
+		glEnd();
+	}
+
+	if (isConfident(toPos)) {
+		glBegin(GL_POINTS);
+		glVertex3fv(XV3toM3D(toPos.position));
+		glEnd();
+	}
+}
