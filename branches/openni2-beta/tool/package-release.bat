1faa8f1657fa1e55e7201ac6b24e2aa4b4fa33fd
tomoto <tomoto@71011c23-f5df-4f85-535c-b37ab9e39a43>
2013-01-14 01:48:23 +0000

    Added release packager for OpenNI2.

--- branches/openni2-beta/tool/package-release.bat
+++ branches/openni2-beta/tool/package-release.bat
@@ -1,7 +1,7 @@
 @rem Note: (1) Set VER to release version. (2) Install ActivePerl.
 
 setlocal
-set VER=dev-1.0b
+set VER=1.0c
 
 set TARGET=release\kinect-ultra_%VER%_for_KinectSDK
 rmdir /s /q %TARGET%
@@ -23,5 +23,21 @@ copy ..\build2010\OpenNI_Release\opencv_*.dll %TARGET%
 copy ..\LICENSE.TXT %TARGET%
 copy ..\README_*.TXT %TARGET%
 copy ..\HISTORY_*.TXT %TARGET%
+perl zip.pl %TARGET%
+
+set TARGET=release\kinect-ultra_%VER%_for_OpenNI2
+rmdir /s /q %TARGET%
+mkdir %TARGET%
+xcopy ..\data %TARGET%\data\ /e
+copy ..\build2010\OpenNI2_Release\kinect-ultra.exe %TARGET%\
+copy ..\build2010\OpenNI2_Release\opencv_*.dll %TARGET%
+copy ..\LICENSE.TXT %TARGET%
+copy ..\README_*.TXT %TARGET%
+copy ..\HISTORY_*.TXT %TARGET%
+copy ..\OpenNI* %TARGET%
+copy ..\PS1080* %TARGET%
+copy ..\NiTE* %TARGET%
+xcopy ..\OpenNI2 %TARGET%\OpenNI2\ /e
+xcopy ..\NiTE2 %TARGET%\NiTE2\ /e
 perl zip.pl %TARGET%
 
